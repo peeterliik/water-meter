6ee9bd79201f7b876558a5edb9261ebdebbd0563
Peeter Liik <liikpeeter@gmail.com>
2022-04-28 19:42:04 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-const express = require('express');
-require('dotenv').config();
-require('./db/mongoose');
-const readingRouter = require('./routers');
+const express = require("express");
+require("dotenv").config();
+require("./db/mongoose");
+const cors = require("cors");
+
+const readingRouter = require("./routers");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3008;
 
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+	})
+);
 app.use(express.json());
 app.use(readingRouter);
 
 app.listen(port, () => {
-  console.log(`Water-meter is listening at http://localhost:${port}`)
+	console.log(`Water-meter is listening at http://localhost:${port}`);
 });
